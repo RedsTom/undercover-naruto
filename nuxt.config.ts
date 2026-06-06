eb2c1b5fa9b45cd5353733e60aa650b1bd688296
RedsTom <tom.frefre.13@gmail.com>
2026-06-06 15:26:55 +0200

    fix: add preset bun in nitro settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,16 @@
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@pinia/nuxt',
-  ],
+  nitro: {
+    preset: "bun",
+  },
 
-  css: ['~/assets/css/main.css'],
+  modules: ["@pinia/nuxt"],
+
+  css: ["~/assets/css/main.css"],
 
   vite: {
     plugins: [tailwindcss()],
@@ -20,11 +22,14 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Undercover Naruto',
+      title: "Undercover Naruto",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Jeu Undercover en ligne sur le thème Naruto' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Jeu Undercover en ligne sur le thème Naruto",
+        },
       ],
     },
   },
